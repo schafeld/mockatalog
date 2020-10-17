d3e00dbf850e02e74acb31c1c98363d28ce3667e
Oliver Schafeld <oliver@schafeld.com>
2020-10-17 17:54:41 +0200

    add HelloWorld Express route

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// [START import]
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+// [END import]
+
+// [START middleware]
+const cors = require('cors')({origin: true});
+app.use(cors);
+// [END middleware]
+
+
+/**
+ * Say hello API
+ * Try: https://us-central1-mockatalog.cloudfunctions.net/api/say/hello
+ */
+app.get('/say/hello', (req, res) => {
+  // Return success response
+  return res.status(200).json({"message":"Hello there... Welcome to Mockatalog server."});
+});
+
+
+// Define the Firebase function that will act as Express application
+// Note: This `api` must match with `/firebase.json` rewrites rule.
+exports.api = functions.https.onRequest(app);
